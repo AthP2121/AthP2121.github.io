12693b075f9bfd62a9f0c6173169f67c1129675f
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-05-25 10:17:22 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ window.onload = function() {
     'IMG_1736.JPG',
     'IMG_1733.JPG',
     'IMG_1737.JPG',
-    'IMG_7242.JPEG',
-    'IMG_7780.JPEG',
-    'IMG_7791.JPEG',
-    'IMG_7925.JPEG',
+    'IMG_7242.JPG',
+    'IMG_7780.JPG',
+    'IMG_7791.JPG',
+    'IMG_7925.JPG',
     'IMG_1739.JPG',
     'IMG_1732.JPG',
     // add more filenames as needed
   ];
 
-  const repoUrl = 'https://AthP2121.github.io/AthP2121.github.io/';
+  const repoUrl = 'https://AthP2121.github.io/AthP2121.github.io/images/';
 
   const feed = document.getElementById('instagram-feed');
   imageNames.forEach(name => {
