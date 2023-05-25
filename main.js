cf8b2581804829e8e9ce603c57be80ea08870a76
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-05-25 10:11:13 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.onload = function() {
   const imageNames = [
     'IMG_1736.JPG',
-    'IMG_1736.JPG',
+    'IMG_1733.JPG',
     'IMG_1737.JPG',
-    'IMG_7242.JPG',
-    'IMG_7780.JPG',
-    'IMG_7791.JPG',
-    'IMG_7925.JPG',
+    'IMG_7242.JPEG',
+    'IMG_7780.JPEG',
+    'IMG_7791.JPEG',
+    'IMG_7925.JPEG',
     'IMG_1739.JPG',
     'IMG_1732.JPG',
     // add more filenames as needed
