492dd6825375626b2ae67b4948c4e36190f1261b
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-05-25 10:22:03 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 window.onload = function() {
   const imageNames = [
-    'IMG_1736.JPG',
-    'IMG_1733.JPG',
-    'IMG_1737.JPG',
-    'IMG_1739.JPG',
-    'IMG_1732.JPG',
+//     'IMG_1736.JPG',
+//     'IMG_1733.JPG',
+//     'IMG_1737.JPG',
+    'IMG_7242.JPG',
+    'IMG_7780.JPG',
+    'IMG_7791.JPG',
+    'IMG_7925.JPG',
+//     'IMG_1739.JPG',
+//     'IMG_1732.JPG',
     // add more filenames as needed
   ];
 
@@ -25,4 +29,3 @@ window.onresize = function() {
     img.style.width = window.innerWidth < 900 ? '100%' : 'calc(50% - 40px)';
   });
 }
-
