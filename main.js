a173e8d852f9925c22970f63e4f63474286a1216
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-08-13 23:14:10 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
+        // './IMG_1736.JPG',
+        // './IMG_1733.JPG',
+        // './IMG_1737.JPG',
+        // './IMG_7242.JPG',
+        // './IMG_7780.JPG',
+        // './IMG_7791.JPG',
+        // './IMG_7925.JPG',
+        // './IMG_1739.JPG',
+        // './IMG_1732.JPG'
+
 document.addEventListener("DOMContentLoaded", function() {
-    const imageNames = [
-        './IMG_1736.JPG',
-        './IMG_1733.JPG',
-        './IMG_1737.JPG',
-        './IMG_7242.JPG',
-        './IMG_7780.JPG',
-        './IMG_7791.JPG',
-        './IMG_7925.JPG',
-        './IMG_1739.JPG',
-        './IMG_1732.JPG'
+    const images = [
+        { src: './IMG_1736.JPG', desc: 'Elegant Gold Ring', price: '$150' },
+        { src: './IMG_1733.JPG', desc: 'Silver Diamond Ring' },
+        // ... other images
     ];
 
     const repoUrl = 'images/';
     const gallery = document.getElementById('image-gallery');
     const fragment = document.createDocumentFragment();
 
-    imageNames.forEach(name => {
+    images.forEach(image => {
+        const div = document.createElement('div');
+        div.className = 'gallery-item';
+        
         const img = document.createElement('img');
-        img.src = repoUrl + name;
-        img.alt = `Gallery image - ${name.split('/').pop().split('.')[0]}`;  // This adds a prefix "Gallery image -" before the filename as the alt text.
-        img.className = "gallery-item";
-        img.loading = "lazy";  // Enable lazy loading
-        img.onerror = function() {
-            img.src = 'path_to_placeholder_image.jpg';
-            img.alt = 'Placeholder image';
-        };
+        img.src = repoUrl + image.src;
+        img.alt = image.desc;
+        img.loading = "lazy";
 
-        fragment.appendChild(img);
+        const caption = document.createElement('p');
+        caption.textContent = image.desc;
+
+        div.appendChild(img);
+        div.appendChild(caption);
+
+        if (image.price) {
+            const priceTag = document.createElement('span');
+            priceTag.className = 'price-tag';
+            priceTag.textContent = image.price;
+            div.appendChild(priceTag);
+        }
+
+        fragment.appendChild(div);
     });
 
     gallery.appendChild(fragment);
