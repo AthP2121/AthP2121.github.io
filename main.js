04a2f5f3b6e09e9f9d985ef71173769cdc17e1e8
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-08-13 23:09:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-window.onload = function() {
-  const imageNames = [
-    './IMG_1736.JPG',
-    './IMG_1733.JPG',
-    './IMG_1737.JPG',
-    './IMG_7242.JPG',
-    './IMG_7780.JPG',
-    './IMG_7791.JPG',
-    './IMG_7925.JPG',
-    './IMG_1739.JPG',
-    './IMG_1732.JPG',
-    // add more filenames as needed
-  ];
+document.addEventListener("DOMContentLoaded", function() {
+    const imageNames = [
+        './IMG_1736.JPG',
+        './IMG_1733.JPG',
+        './IMG_1737.JPG',
+        './IMG_7242.JPG',
+        './IMG_7780.JPG',
+        './IMG_7791.JPG',
+        './IMG_7925.JPG',
+        './IMG_1739.JPG',
+        './IMG_1732.JPG'
+    ];
 
-  const repoUrl = 'images/';
+    const repoUrl = 'images/';
+    const gallery = document.getElementById('image-gallery');
+    const fragment = document.createDocumentFragment();
 
-  const gallery = document.getElementById('image-gallery');
-  imageNames.forEach(name => {
-    const img = document.createElement('img');
-    img.src = repoUrl + name;
-    img.className = "gallery-item";
-    gallery.appendChild(img);
-  });
-}
+    imageNames.forEach(name => {
+        const img = document.createElement('img');
+        img.src = repoUrl + name;
+        img.alt = `Gallery image - ${name.split('/').pop().split('.')[0]}`;  // This adds a prefix "Gallery image -" before the filename as the alt text.
+        img.className = "gallery-item";
+        img.loading = "lazy";  // Enable lazy loading
+        img.onerror = function() {
+            img.src = 'path_to_placeholder_image.jpg';
+            img.alt = 'Placeholder image';
+        };
+
+        fragment.appendChild(img);
+    });
+
+    gallery.appendChild(fragment);
+});
